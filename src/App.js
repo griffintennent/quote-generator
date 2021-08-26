df11cf165c16b861eef05c36fcb6062541ae4d1d
griffintennent <griffintennent@gmail.com>
2021-08-26 18:48:39 -0400

    renamed main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function QuoteGeneratorCard() {
   return (
     <div className="quote-box">
       <button>MyButton</button>
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+export default QuoteGeneratorCard;
