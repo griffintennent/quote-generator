c970735d42f898644c581e36d9bc641b49f73aa2
griffintennent <griffintennent@gmail.com>
2021-08-26 18:34:38 -0400

    init. create-react-app with sample project trimmed out

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="quote-box">
+      <button>MyButton</button>
+    </div>
+  );
+}
+
+export default App;
