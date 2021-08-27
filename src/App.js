df4a1e337a1b4ba91bf34b3ead4def928fce735d
griffintennent <griffintennent@gmail.com>
2021-08-26 22:20:14 -0400

    Rest of the HTML elements, some precursory CSS, state+quotes NYI

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import './App.css';
 
 function QuoteGeneratorCard() {
   return (
-    <div className="quote-box">
-      <button>MyButton</button>
-    </div>
+    <body>
+      <div id="wrapper">
+      <div id="quote-box">
+        <div class="quote-text">
+          <span id="text">Someone Wrote a Quote here. Lorum Ipsum blah blah blah blah blah filling up the element</span>
+          <span id="author">-Author</span>
+          <button id="new-quote">New Quote</button>
+          <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet this quote</a>
+        </div>
+      </div>
+      </div>
+    </body>
   );
 }
 
